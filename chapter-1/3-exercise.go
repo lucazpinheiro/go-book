4ce7d7bc931a7ab1f8ced2c11cb0c26eb7337fb3
Lucas <lucascpinheiro10@gmail.com>
2021-12-15 22:17:27 -0300

    add .gitignore, examples and exercise from section 3

--- chapter-1/3-exercise.go
+++ chapter-1/3-exercise.go
@@ -1,3 +1,4 @@
+// print the names of all files in which each duplic ated line occ urs.
 package main
 
 import (
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	counts := make(map[string]int)
+	filesWithDuplicatedLines := make(map[string]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
@@ -18,13 +20,18 @@ func main() {
 				fmt.Fprint(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			input := bufio.NewScanner(f)
+			for input.Scan() {
+				filesWithDuplicatedLines[input.Text()] = arg
+				counts[input.Text()]++
+			}
+			// countLines(f, counts, filesWithDuplicatedLines)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%s\n", n, line, filesWithDuplicatedLines[line])
 		}
 	}
 }
